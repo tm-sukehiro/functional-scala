8b910a8fcbefa25acbd33ee4258e170255c31e43
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-07-08 18:33:28 +0900

    8.2.4まで完了

---
+++ src/main/scala/testing/Gen.scala
@@ -0,0 +1,66 @@
+package testing
+
+import state._
+
+case class Gen[+A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] =
+    Gen(sample.map(f))
+
+  def map2[B, C](g: Gen[B])(f: (A, B) => C): Gen[C] =
+    Gen(sample.map2(g.sample)(f))
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size flatMap (n => this.listOfN(n))
+
+}
+
+object Gen {
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  val boolean: Gen[Boolean] =
+    Gen(State(RNG.boolean))
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+
+  val uniform: Gen[Double] = Gen(State(RNG.double))
+
+  def choose(i: Double, j: Double): Gen[Double] =
+    Gen(State(RNG.double).map(d => i + d * (j - i)))
+
+  def even(start: Int, stopExclusive: Int): Gen[Int] =
+    choose(start, if (stopExclusive % 2 == 0) stopExclusive - 1 else stopExclusive).
+      map (n => if (n % 2 != 0) n + 1 else n)
+
+  def odd(start: Int, stopExclusive: Int): Gen[Int] =
+    choose(start, if (stopExclusive % 2 != 0) stopExclusive - 1 else stopExclusive).
+      map (n => if (n % 2 == 0) n + 1 else n)
+
+  def sameParity(from: Int, to: Int): Gen[(Int, Int)] = for {
+    i <- choose(from, to)
+    j <- if (i % 2 == 0) even(from, to) else odd(from, to)
+  } yield (i, j)
+
+  def listOfN_1[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    List.fill(n)(g).foldRight(unit(List[A]()))((a, b) => a.map2(b)(_ :: _))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap(b => if (b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+
+    Gen(State(RNG.double).flatMap(d => if ( d < g1Threshold) g1._1.sample else g2._1.sample))
+  }
+
+}
